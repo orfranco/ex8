baa0e1e99537e140d5ecd26f26bde7baeb14b11a
nimrodnm <nimrod.bargiora@mail.huji.ac.il>
2020-12-10 11:59:52 +0200

    started writing tests

--- test_nonogram.py
+++ test_nonogram.py
@@ -1,1 +1,9 @@
 from nonogram import *
+
+
+def test_constraint_satisfactions():
+    assert sorted(constraint_satisfactions(3, [1])) == sorted([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    assert sorted(constraint_satisfactions(3, [2])) == sorted([[1, 1, 0], [0, 1, 1]])
+    assert sorted(constraint_satisfactions(3, [1, 1])) == sorted([[1, 0, 1]])
+    assert sorted(constraint_satisfactions(4, [1, 1])) == sorted([[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1]])
+    assert sorted(constraint_satisfactions(5, [2, 1])) == sorted([[1, 1, 0, 1, 0] , [1, 1, 0, 0, 1], [0, 1, 1, 0, 1]])
